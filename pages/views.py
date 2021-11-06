8155767b6fab10d5d053a8db7d6fba04beec6ce4
Sayantan Mandal <sanu_xze@yahoo.com>
2021-11-06 16:23:32 +0530

    medium part-1 first commit

---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+# homepage
+def homepage_view(request):
+    return render(request, 'homepage.html', {})
+
+# about page
+def about_view(request):
+    return render(request, 'about.html', {})
